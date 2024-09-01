298e45a993adbf03dd5a3c86931d948d14dec0e1
noname33 <koalal1428@gmail.com>
2024-09-01 15:34:58 +0300

    Add pyrogram client class create

---
+++ breadixchattg/pyrogram_tools/client.py
@@ -0,0 +1,10 @@
+from pyrogram import Client
+
+from breadixchattg.core.config import settings
+
+client = Client(
+    name='BreadixChatTGApp',
+    api_id=settings.tg_app.id,
+    api_hash=settings.tg_app.hash,
+    bot_token=settings.tg_api_keys.bot_api_key
+)
