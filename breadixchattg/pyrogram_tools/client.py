0194e9345277a49b73f989a8037b790598bd88cb
noname33 <koalal1428@gmail.com>
2024-09-01 15:39:55 +0300

    Add no_updates flag to pyrogram client

--- breadixchattg/pyrogram_tools/client.py
+++ breadixchattg/pyrogram_tools/client.py
@@ -6,5 +6,6 @@ client = Client(
     name='BreadixChatTGApp',
     api_id=settings.tg_app.id,
     api_hash=settings.tg_app.hash,
-    bot_token=settings.tg_api_keys.bot_api_key
+    bot_token=settings.tg_api_keys.bot_api_key,
+    no_updates=True
 )
